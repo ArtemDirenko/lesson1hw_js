ffe1915af6dd909dcf86d707f99c8ab0aad2e90b
“ArtemDirenko” <direnko28@gmail.com>
2022-11-02 00:58:02 +0200

    lesson1hw

---
+++ main.js
@@ -0,0 +1,35 @@
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+console.log(`${s1} ${s2} ${s3} ${s4}`)
+let num = 1;
+console.log(num)
+num = 10;
+console.log(num)
+num = -999;
+console.log(num)
+num = 123;
+console.log(num)
+num = 3.14;
+console.log(num)
+let z = 100<300;
+console.log(z);
+let z2 = 100>300;
+console.log(z2)
+
+let user = {
+    firstName: 'Artem',
+    middleName: 'Yriiovich',
+    lastName: 'Direnko'
+};
+console.log(user)
+
+
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+ console.log(typeof b)
+let c = true;
+console.log(typeof c)
